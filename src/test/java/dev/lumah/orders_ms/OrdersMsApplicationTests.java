57bade9d23b78f59fe45d7951f332b9127a0122b
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-08-06 13:40:08 -0300

    feat project

---
+++ src/test/java/dev/lumah/orders_ms/OrdersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lumah.orders_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
