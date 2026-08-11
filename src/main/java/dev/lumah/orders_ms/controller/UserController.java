6cb34685a62d772630ad76e9c48aa06da3f032c0
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-08-11 16:43:44 -0300

    fix

--- src/main/java/dev/lumah/orders_ms/controller/UserController.java
+++ src/main/java/dev/lumah/orders_ms/controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponse createUser(@RequestBody @Valid CreateUserRequest dto) {
         UserResponse response = userService.createUser(dto);
-        rabbitTemplate.convertAndSend("queue.user.validateEmail", response);
+        rabbitTemplate.convertAndSend("queue.user.validateEmail", response.id());
         return response;
     }
 
