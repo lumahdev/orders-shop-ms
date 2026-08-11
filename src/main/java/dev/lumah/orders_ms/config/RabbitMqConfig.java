ab10fee8b387735c8287f00fc741d50bf83ee37b
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-08-11 10:18:29 -0300

    feat rabbit order queue

--- src/main/java/dev/lumah/orders_ms/config/RabbitMqConfig.java
+++ src/main/java/dev/lumah/orders_ms/config/RabbitMqConfig.java
@@ -1,12 +1,33 @@
 package dev.lumah.orders_ms.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.support.converter.DefaultJacksonJavaTypeMapper;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
-    public Queue createQueue() {
-        return new Queue("");
+    public static final String ORDER_PAYMENT_PENDING = "order.payment.pending";
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        JacksonJsonMessageConverter converter = new JacksonJsonMessageConverter();
+
+        DefaultJacksonJavaTypeMapper typeMapper = new DefaultJacksonJavaTypeMapper();
+        typeMapper.setTrustedPackages("dev.lumah.orders_ms.dto");
+
+        converter.setJavaTypeMapper(typeMapper);
+
+        return converter;
+    }
+
+    @Bean
+    Queue orderPaymentPending() {
+        return QueueBuilder
+                .durable(ORDER_PAYMENT_PENDING)
+                .build();
     }
 }
--- src/main/java/dev/lumah/orders_ms/controller/OrderController.java
+++ src/main/java/dev/lumah/orders_ms/controller/OrderController.java
@@ -4,11 +4,14 @@ import dev.lumah.orders_ms.dto.CreateOrderRequest;
 import dev.lumah.orders_ms.dto.OrderResponse;
 import dev.lumah.orders_ms.service.OrderService;
 import jakarta.validation.Valid;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static dev.lumah.orders_ms.config.RabbitMqConfig.ORDER_PAYMENT_PENDING;
 
 @RestController
 @RequestMapping("/orders")
@@ -17,10 +20,15 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrderResponse createOrder(@RequestBody @Valid CreateOrderRequest dto) {
-        return orderService.createOrder(dto);
+        OrderResponse response = orderService.createOrder(dto);
+        rabbitTemplate.convertAndSend(ORDER_PAYMENT_PENDING, response);
+        return response;
     }
 
     @GetMapping
