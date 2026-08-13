f51b638c4ee3ea81c6ab401d5ccd2dd7bc8192bc
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-08-13 12:19:58 -0300

    fix

--- src/main/java/dev/lumah/orders_ms/exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/lumah/orders_ms/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package dev.lumah.orders_ms.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
@@ -12,11 +14,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        
+        ResponseStatus annotation = ex.getClass().getAnnotation(ResponseStatus.class);
+        if (annotation != null) {
+            status = annotation.value();
+        }
+        
         Map<String, Object> body = Map.of(
             "error", ex.getClass().getSimpleName(),
             "message", ex.getMessage(),
             "timestamp", LocalDateTime.now()
         );
-        return ResponseEntity.internalServerError().body(body);
+        return ResponseEntity.status(status).body(body);
     }
 }
